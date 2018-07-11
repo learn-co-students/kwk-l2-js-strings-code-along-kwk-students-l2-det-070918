04c3f71d46c0b0db8a574e770a5dd1d15b87914b
cristinanoelle11 <cristina.powers@powerstribe.net>
2018-07-11 17:43:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 // Write your code in this file!
+var currentUser = 'Grace Hopper';
+var welcomeMessage = `Welcome to Klossybook, ${currentUser}!`;
 
+var excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+var shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
